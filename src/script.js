88f18d6d942331a647bffb1866f2d46529299b6d
Windso <kristianwindso@gmail.com>
2017-09-10 23:08:15 -0700

    basically rewrote the entire script

--- src/script.js
+++ src/script.js
@@ -14,12 +14,12 @@ var adjectives = ["used","every","large","popular","able","basic","known","vario
 var nouns = ["people","history","way","art","world","map","two","family","health","system","meat","year","thanks","music","person","reading","method","data","food","theory","law","bird","problem","control","power","ability","love","science","library","nature","fact","product","idea","area","society","story","media","thing","oven","safety","quality","player","variety","video","week","country","exam","movie","physics","policy","series","thought","basis","army","camera","freedom","paper","child","month","truth","writing","article","goal","news","fishing","growth","income","user","failure","meaning","teacher","night","disease","disk","energy","nation","road","role","soup","success","math","moment","event","student","wood","office","unit","context","driver","flight","length","cell","dealer","finding","lake","member","message","phone","scene","concept","death","housing","mood","woman","advice","blood","effort","opinion","payment","reality","skill","wealth","city","county","depth","estate","heart","photo","recipe","studio","topic","passion","setting","ad","agency","college","debt","memory","aspect","storage","version","alcohol","highway","loss","steak","union","cancer","entry","mixture","region","virus","actor","device","drama","engine","hotel","owner","tension","anxiety","bath","bread","climate","emotion","guest","height","mall","manager","sample","charity","cousin","editor","extent","guitar","leader","mom","outcome","revenue","session","singer","tennis","basket","bonus","cabinet","church","clothes","coffee","dinner","drawing","hair","hearing","lab","mode","mud","orange","poetry","police","queen","ratio","sector","song","tooth","town","vehicle","volume","wife","airport","arrival","chapter","error","farmer","gate","girl","hall","injury","meal","pie","poem","river","son","speech","tea","village","warning","winner","worker","writer","breath","buyer","chest","cookie","courage","dad","desk","drawer","garbage","grocery","honey","insect","king","ladder","menu","penalty","piano","potato","salad","sister","tongue","wedding","affair","analyst","apple","bedroom","beer","cheek","client","diamond","dirt","ear","fortune","funeral","gene","hat","lady"];
 
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "1234567890";
 var symbols = "!@#$%^&*()+=-";
 
 var allChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()+-=[];,./?";
 
 // generate passwords
+var passwordsMaster = [];
 function generatePasswords() {
 	hasGenerated = true;
 	var i = 0,
@@ -27,10 +27,11 @@ function generatePasswords() {
 
 	div.innerHTML = "";
 	while (i < 3) {
+		passwordsMaster[i] = createPassword();
 		if (isMobile) {
-			div.innerHTML = div.innerHTML + '<p class="password">' + getPassword() + '</p>';
+			div.innerHTML = div.innerHTML + '<p id="p' + i + '" class="password">' + customize(passwordsMaster[i]) + '</p>';
 		} else {
-			div.innerHTML = div.innerHTML + '<input type="text" class="password" value="' + getPassword() + '" onClick="select()" maxlength="32" spellcheck="false" /><br>';
+			div.innerHTML = div.innerHTML + '<input type="text" id="p' + i + '" class="password" value="' + customize(passwordsMaster[i]) + '" onClick="select()" maxlength="32" spellcheck="false" /><br>';
 		}
 		i++;
 	}
@@ -40,77 +41,108 @@ function generatePasswords() {
 function refresh() {
 	document.getElementById('lengthDisplay').innerHTML = document.getElementById('length').value;
 	if (hasGenerated) {
-		generatePasswords();
+		if (isMobile) {
+			document.getElementById("p0").innerHTML = customize(passwordsMaster[0]);
+			document.getElementById("p1").innerHTML = customize(passwordsMaster[1]);
+			document.getElementById("p2").innerHTML = customize(passwordsMaster[2]);
+		} else {
+			document.getElementById("p0").setAttribute("value", customize(passwordsMaster[0]));
+			document.getElementById("p1").setAttribute("value", customize(passwordsMaster[1]));
+			document.getElementById("p2").setAttribute("value", customize(passwordsMaster[2]));
+		}
 	}
 }
 
 // generate single password
-function getPassword() {
-	var count = 0,
-		wordyPassword = "",
-		notWordyPassword = "",
-		passLength = document.getElementById('length').value;
+function createPassword() {
+	var password = {},
+		passwordLength = 0;
 
 	// Generate wordy password
-    while (wordyPassword.length != passLength) {
-    	count++;
-    	wordyPassword = "";
+    while (passwordLength != 27) { // TODO: also make it look nice at the currently set length
+    	password.verb = verbs[Math.floor(Math.random() * verbs.length)];
+    	password.adj1 = adjectives[Math.floor(Math.random() * adjectives.length)];
+    	password.adj2 = adjectives[Math.floor(Math.random() * adjectives.length)];
+    	password.adj3 = adjectives[Math.floor(Math.random() * adjectives.length)];
+    	password.noun = nouns[Math.floor(Math.random() * nouns.length)];
+    	password.num = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
 
-    	// add verb if 8+ char long
-    	if (passLength > 8) {
-			wordyPassword = verbs[Math.floor(Math.random() * verbs.length)] + "-";
-    	}
-
-    	// add adjective(s) if 11+ char long
-    	if (passLength > 13) {
-			for (var i = 0; i < (passLength-7)/11; ++i) {
-				wordyPassword += adjectives[Math.floor(Math.random() * adjectives.length)] + "-";
-			}
-		}
-
-		// add noun and number
-		var noun = nouns[Math.floor(Math.random() * nouns.length)];
-    	if (passLength < 5) {
-			noun = noun.substr(0,passLength-1);
-		}
-		wordyPassword += noun + numbers[Math.floor(Math.random() * numbers.length)];
-
-		// proper grammar
-    	wordyPassword = wordyPassword.charAt(0).toUpperCase() + wordyPassword.slice(1);
+    	passwordLength = password.verb.length + password.adj1.length + password.adj2.length + password.adj3.length + password.noun.length;
 	}
 
-    // Do character replacements
-    //
-    // TODO
-    //
+	return password;
+}
 
-    // Generate not wordy password
-	for (var i = 0, n = allChars.length; i < passLength; ++i) {
+// apply settings to password
+function customize(oldPassword) {
+	var newPassword,
+		passLength = document.getElementById('length').value,
+		wordStructure = document.getElementById('wordStructure').value;
+
+	// apply length
+	if (passLength < oldPassword.verb.length +1) {
+		newPassword = oldPassword.verb.substr(0, passLength-1);
+	} else {
+		newPassword = oldPassword.verb + "-" + oldPassword.adj1 + "-" + oldPassword.adj2 + "-" + oldPassword.adj3 + "-" + oldPassword.noun;
+		if (passLength != 32) {
+			var pos = newPassword.substr(0, passLength).lastIndexOf("-");
+			newPassword = newPassword.substr(0, pos) + numberize(newPassword.substr(pos));
+			newPassword = newPassword.substr(0, passLength-1);
+		}
+	}
+
+	
+
+	// apply word structure
+	/*for (var i = 0, n = allChars.length; i < passLength; ++i) {
         notWordyPassword += allChars.charAt(Math.floor(Math.random() * n));
-    }
+    }*/
 
-    console.log("took " + count + " tries to make " + passLength + " char password: " + wordyPassword);
+	// capitalize first letter
+	newPassword = newPassword.charAt(0).toUpperCase() + newPassword.slice(1);
 
-    // Mix wordy with not wordy
-    if (randomChance(document.getElementById('wordStructure').value)) {
-	    return wordyPassword;
-    } else {
-	    return notWordyPassword;
-	}
+	// add number
+	newPassword += oldPassword.num;
+
+	// done
+	return newPassword;
 }
 
-// random number
-function generateRandomNumber(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+var mapObj = {
+	'-': '0',
+	'a': '4',
+	'b': '6',
+	'c': '3',
+	'd': '0',
+	'e': '3',
+	'f': '7',
+	'g': '9',
+	'h': '8',
+	'i': '1',
+	'j': '7',
+	'k': '5',
+	'l': '1',
+	'm': '3',
+	'n': '2',
+	'o': '0',
+	'p': '9',
+	'q': '9',
+	'r': '4',
+	's': '5',
+	't': '1',
+	'u': '2',
+	'v': '4',
+	'w': '3',
+	'x': '8',
+	'y': '7',
+	'z': '5'
 }
+var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
 
-// return percentage
-function randomChance(percent) {
-    if (generateRandomNumber(0,10) <= percent) {
-    	return true;
-    } else {
-    	return false;
-    }
+function numberize(words) {
+	return words.replace(re, function(matched){
+		return mapObj[matched];
+	});
 }
 
 
